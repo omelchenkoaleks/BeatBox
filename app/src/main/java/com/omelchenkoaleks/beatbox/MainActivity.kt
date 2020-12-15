7536cf5a18104c59681fe3530c5b5116d40f6d97
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-15 15:18:19 +0200

    Использование привязки для MainActivity.kt

--- app/src/main/java/com/omelchenkoaleks/beatbox/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/beatbox/MainActivity.kt
@@ -2,10 +2,14 @@ package com.omelchenkoaleks.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.omelchenkoaleks.beatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
