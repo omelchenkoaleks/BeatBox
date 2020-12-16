00f01ed97662f3746767a7592839c837a949a8d6
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-16 11:37:14 +0200

    Создание объекта SoundPool, который будет использоваться для воспроизведения звуков.

--- app/src/main/java/com/omelchenkoaleks/beatbox/BeatBox.kt
+++ app/src/main/java/com/omelchenkoaleks/beatbox/BeatBox.kt
@@ -1,14 +1,20 @@
 package com.omelchenkoaleks.beatbox
 
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 class BeatBox(private val assets: AssetManager) {
 
     val sounds: List<Sound>
+
+    private val soundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
 
     init {
         sounds = loadSounds()
